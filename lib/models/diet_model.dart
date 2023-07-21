07b890736071b2a7849490bd333d2c8d4463c0fb
diana <dinazhusubalieva@gmail.com>
2023-07-21 10:21:19 +0600

    fitness layout

--- lib/models/diet_model.dart
+++ lib/models/diet_model.dart
@@ -9,43 +9,44 @@ class DietModel {
   Color boxColor;
   bool viewIsSelected;
 
-  DietModel({
-    required this.name,
-    required this.iconPath,
-    required this.level,
-    required this.duration,
-    required this.calorie,
-    required this.boxColor,
-    required this.viewIsSelected
-  });
+  DietModel(
+      {required this.name,
+      required this.iconPath,
+      required this.level,
+      required this.duration,
+      required this.calorie,
+      required this.boxColor,
+      required this.viewIsSelected});
 
-  static List < DietModel > getDiets() {
-    List < DietModel > diets = [];
+  static List<DietModel> getDiets() {
+    List<DietModel> diets = [];
 
-    diets.add(
-      DietModel(
-       name: 'Honey Pancake',
-       iconPath: 'assets/icons/honey-pancakes.svg',
-       level: 'Easy',
-       duration: '30mins',
-       calorie: '180kCal',
-       viewIsSelected: true,
-       boxColor: Color(0xff9DCEFF)
-      )
-    );
+    diets.add(DietModel(
+        name: 'Honey Pancake',
+        iconPath: 'assets/icons/honey-pancakes.svg',
+        level: 'Easy',
+        duration: '30mins',
+        calorie: '180kCal',
+        viewIsSelected: true,
+        boxColor: Color(0xff9DCEFF)));
 
-    diets.add(
-      DietModel(
-       name: 'Canai Bread',
-       iconPath: 'assets/icons/canai-bread.svg',
-       level: 'Easy',
-       duration: '20mins',
-       calorie: '230kCal',
-       viewIsSelected: false,
-       boxColor: Color(0xffEEA4CE)
-      )
-    );
+    diets.add(DietModel(
+        name: 'Canai Bread',
+        iconPath: 'assets/icons/canai-bread.svg',
+        level: 'Easy',
+        duration: '20mins',
+        calorie: '230kCal',
+        viewIsSelected: false,
+        boxColor: Color(0xffEEA4CE)));
 
+    diets.add(DietModel(
+        name: 'Cupcake',
+        iconPath: 'assets/icons/honey-pancakes.svg',
+        level: 'Easy',
+        duration: '30mins',
+        calorie: '430kCal',
+        viewIsSelected: true,
+        boxColor: Color(0xff9DCEFF)));
     return diets;
   }
 }
